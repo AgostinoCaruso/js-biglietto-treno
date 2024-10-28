4275b960d14e7768b1c5be81a5f8b7d98373b324
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-10-28 15:35:52 +0100

    add user input for km

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("ciao");
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+- L'output del prezzo finale va messo fuori in forma umana 
+(con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*/
+
+// variables
+const priceTicketKm = 0.21; //for each km €
+const discountJunior = 20; //is a percentage discount for below 18 y
+const discountSenior = 40; //is a percentage discount for over 65 y
+let finalPrice; //is the final price, using fixed to cut decimal num
+
+//user input
+let kmForTrip, userAge; // km for the travel, age of the user input
+console.clear();
+//asking user age and km
+do{
+    kmForTrip = parseInt(prompt("Hi, insert your travel km"));
+}while(isNaN(kmForTrip)) // i make sure user can only input num
+
+console.log(kmForTrip);
